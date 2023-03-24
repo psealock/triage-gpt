848503327cbd7aafc02985ee675b07388b48315c
Paul Sealock <psealock@gmail.com>
2023-03-24 14:18:24 +1300

    clean issue bodies

--- index.js
+++ index.js
@@ -4,23 +4,41 @@ const octokit = new Octokit({
 	auth: process.env.GITHUB_TOKEN,
 });
 
+const cleanIssueBody = (body) => {
+	return (
+		body
+			// Remove environment info
+			.replace(/### WordPress Environment(.|\t|\r|\n|\f)*/g, '')
+			// Remove prerequisites
+			.replace(/### Prerequisites(.|\t|\r|\n|\f)*duplicate\.\n/, '')
+	);
+};
+
 const getIssues = async () => {
 	const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
 		owner: 'woocommerce',
 		repo: 'woocommerce',
-		per_page: 1,
+		per_page: 10,
 		state: 'closed',
 	});
 
-	return data.map((issue) => {
-		return {
-			number: issue.number,
-			body: issue.body,
-			labels: issue.labels.map((label) => label.id), // filter out needs: triage etc
-		};
-	});
+	return (
+		data
+			// ignore pull requests
+			.filter((issue) => {
+				return issue.pull_request === undefined;
+			})
+			.map((issue) => {
+				return {
+					number: issue.number,
+					body: cleanIssueBody(issue.body),
+					labels: issue.labels.map((label) => label.id), // filter out needs: triage etc
+				};
+			})
+	);
 };
 
 getIssues().then(console.log);
+// getIssues();
 
 // console.log(process.env.OPENAI_API_KEY);
