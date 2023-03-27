2fbc5f803f7110543684a48949113f9d4b7e6554
Paul Sealock <psealock@gmail.com>
2023-03-27 18:04:46 +1300

    WP formatting

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-const { Octokit } = require('octokit');
+const { Octokit } = require( 'octokit' );
 
-const octokit = new Octokit({
+const octokit = new Octokit( {
 	auth: process.env.GITHUB_TOKEN,
-});
+} );
 
-const cleanIssueBody = (body) => {
+const labelsToFilterOut = [];
+
+const cleanIssueBody = ( body ) => {
 	return (
 		body
 			// Remove environment info
-			.replace(/### WordPress Environment(.|\t|\r|\n|\f)*/g, '')
+			.replace( /### WordPress Environment(.|\t|\r|\n|\f)*/g, '' )
 			// Remove prerequisites
-			.replace(/### Prerequisites(.|\t|\r|\n|\f)*duplicate\.\n/, '')
+			.replace( /### Prerequisites(.|\t|\r|\n|\f)*duplicate\.\n/, '' )
 	);
 };
 
 const getIssues = async () => {
-	const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-		owner: 'woocommerce',
-		repo: 'woocommerce',
-		per_page: 10,
-		state: 'closed',
-	});
+	const { data } = await octokit.request(
+		'GET /repos/{owner}/{repo}/issues',
+		{
+			owner: 'woocommerce',
+			repo: 'woocommerce',
+			per_page: 10,
+			state: 'closed',
+		}
+	);
 
 	return (
 		data
 			// ignore pull requests
-			.filter((issue) => {
+			.filter( ( issue ) => {
 				return issue.pull_request === undefined;
-			})
-			.map((issue) => {
+			} )
+			.map( ( issue ) => {
 				return {
 					number: issue.number,
-					body: cleanIssueBody(issue.body),
-					labels: issue.labels.map((label) => label.id), // filter out needs: triage etc
+					body: cleanIssueBody( issue.body ),
+					labels: issue.labels.map( ( label ) => label.id ), // filter out needs: triage etc
 				};
-			})
+			} )
 	);
 };
 
-getIssues().then(console.log);
+getIssues().then( console.log );
 // getIssues();
 
 // console.log(process.env.OPENAI_API_KEY);
