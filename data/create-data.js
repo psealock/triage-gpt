047416787abe9842554ded0a2f79affd79176321
Paul Sealock <psealock@gmail.com>
2023-03-28 16:47:52 +1300

    automate manual clean

--- data/create-data.js
+++ data/create-data.js
@@ -17,6 +17,22 @@ const cleanIssueBody = ( body ) => {
 				/<!-- This form is for other issue types(.|\t|\r|\n|\f)*Issue Description/gm,
 				''
 			)
+			.replace( "### Describe the solution you'd like\r\n\r\n" )
+			.replace( '\n### Describe the bug\n\n', '' )
+			.replace( ':**\r\n\r\n', '' )
+			.replace( '\n### Describe the bug\n\n', '' )
+			.replace( '**Description**\n\n', '' )
+			.replace(
+				'### Prerequisites\r\n\r\n- [X] I have carried out troubleshooting steps and I believe I have found a bug.\r\n- [X] I have searched for similar bugs in both open and closed issues and cannot find a duplicate.\r\n\r\n### Describe the bug\r\n\r\n',
+				''
+			)
+			.replace( '### Description\n\n', '' )
+			.replace( '**Issue Description:**\r\n\r\n', '' )
+			.replace( "### Describe the solution you'd like\n\n", '' )
+			.replace(
+				'### Prerequisites\r\n\r\n- [ ] I have carried out troubleshooting steps and I believe I have found a bug.\r\n- [ ] I have searched for similar bugs in both open and closed issues and cannot find a duplicate.\r\n\r\n',
+				''
+			)
 	);
 };
 
